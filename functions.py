eaeb2524ccc3b274b811d587f614f86a572c6df0
wadezhuang <wzhuang@berklee.edu>
2026-09-25 17:15:15 -0400

    complete assignment

---
+++ functions.py
@@ -0,0 +1,11 @@
+def C_to_f(celsius):
+    #later
+    return fahrenheit
+
+def tax (price,tax_rate):
+    #later
+    return sales tax
+
+define a_b(length):
+    #later
+    return C
---
+++ restaurant.py
@@ -0,0 +1,14 @@
+def grill_pancakes(batter,nuberOfPancake):
+    #i'll write this later
+    return grilled pancakes
+
+
+def make_batter(numberofPancake):
+    #i'll write this later
+    return batter
+    
+def deliver_plates(plate):
+    #i'll write this later
+    print("Enjoy your pancakes!")
+    return none
+    
